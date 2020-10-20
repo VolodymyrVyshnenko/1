8acfdaaae407b1a32d6e848387eb8485ab64f020
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-10-20 21:21:25 +0300

    Добавьте файлы проекта.

---
+++ 1/Adress.cs
@@ -0,0 +1,78 @@
+﻿namespace _1
+{
+    internal class Adress
+    {
+        string index;
+        public string Index
+        {
+            set
+            {
+                index = value;
+            }
+            get
+            {
+                return index;
+            }
+        }
+        private string country;
+        public string Country
+        {
+            set
+            {
+                country = value;
+            }
+            get
+            {
+                return country;
+            }
+        }
+        string city;
+        public string City
+        {
+            set
+            {
+                city = value;
+            }
+            get
+            {
+                return city;
+            }
+        }
+        string street;
+        public string Street
+        {
+            set
+            {
+                street = value;
+            }
+            get
+            {
+                return street;
+            }
+        }
+        string house;
+        public string House
+        {
+            set
+            {
+                house = value;
+            }
+            get
+            {
+                return house;
+            }
+        }
+        string apartment;
+        public string Apartment
+        {
+            set
+            {
+                apartment = value;
+            }
+            get
+            {
+                return apartment;
+            }
+        }
+    }
+}
---
+++ 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress program = new Adress();
+
+            program.Index = "02000";
+            Console.WriteLine(program.Index);
+
+            program.Country = "Ukraine";
+            Console.WriteLine(program.Country);
+
+            program.City = "Kyiv";
+            Console.WriteLine(program.City);
+
+            program.Street = "Lobachevskogo";
+            Console.WriteLine(program.Street);
+
+            program.House = "23";
+            Console.WriteLine(program.House);
+
+            program.Apartment = "350";
+            Console.WriteLine(program.Apartment);
+
+            Console.ReadKey();
+        }
+    }
+}
